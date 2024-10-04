f2d80cf6107333562b7ea5c67029cd4e23c1f30a
sashabelyakova <aleksandraassistant23@gmail.com>
2024-10-04 13:01:08 +0300

    Lesson_5: Work with cycles

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Employee {
+    private String fio;
+    private String jobTitle;
+    private String email;
+    private String phone;
+    private String salary;
+    private Integer age;
+
+    public Employee(String fio, String jobTitle, String email, String phone,String salary, Integer age) {
+        this.fio = fio;
+        this.jobTitle = jobTitle;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInformation() {
+        System.out.println(
+                fio + " " + jobTitle + " " + email + " " + phone + " " + salary + " " + age) ;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,17 @@
 package org.example;
 
 public class Main {
+    private static Employee[] employees = new Employee[5];
+
     public static void main(String[] args) {
+        addEmployeesToArray();
+    }
+
+    private static void addEmployeesToArray() {
+        employees[0] = new Employee("Belyakova AC", "QA", "shkajsh@gmail.com", "+(23)6572656", "10000$", 23);
+        employees[1] = new Employee("Ivanov AC", "QC", "feuhr@gmail.com", "+(23)34534", "11000$", 24);
+        employees[2] = new Employee("Vinov AC", "TO", "fgrht@gmail.com", "+(23)78978", "12000$", 25);
+        employees[3] = new Employee("Soloviov AC", "BA", "tjtyjyt@gmail.com", "+(23)566565", "13000$", 26);
+        employees[4] = new Employee("Lostov AC", "Dev", "ghjyjty@gmail.com", "+(23)122112", "14000$", 27);
     }
 }

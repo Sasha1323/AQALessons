d4858f3983479a187a2e371f479660214bd58354
sashabelyakova <aleksandraassistant23@gmail.com>
2024-10-18 22:23:17 +0300

    Lesson_7_junit_5

---
+++ src/test/java/FactorialCalculatorTest.java
@@ -0,0 +1,25 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FactorialCalculatorTest {
+
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(1, FactorialCalculator.factorial(0));
+    }
+
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        assertEquals(120, FactorialCalculator.factorial(5));
+        assertEquals(720, FactorialCalculator.factorial(6));
+    }
+
+    @Test
+    public void testFactorialOfNegativeNumber() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            FactorialCalculator.factorial(-1);
+        });
+        assertEquals("Число должно быть неотрицательным", exception.getMessage());
+    }
+}

734890006776e52ab5b9d1c4c87e41b31de6eee0
sashabelyakova <aleksandraassistant23@gmail.com>
2024-10-20 22:37:23 +0300

    Lesson_14_junit_5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
+import static org.example.FactorialCalculator.factorial;
 
 public class Main {
     public static void main(String[] args) {
+        int number = 5; // Здесь вы можете изменить число
+        long result = factorial(number);
+        System.out.println("Факториал " + number + " = " + result);
+    }
     }
 }
